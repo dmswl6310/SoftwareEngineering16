cc101678bcb5fa432d4854042ee6dcaa696486f9
Hwang-JS <45121178+Hwang-JS@users.noreply.github.com>
2019-06-19 13:52:33 +0900

    Update Member.java

--- Project_Code/LogIn/Member.java
+++ Project_Code/LogIn/Member.java
@@ -1,12 +1,22 @@
 package LogIn;
 
-public class Member extends User{
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.io.BufferedWriter;
+import java.io.File;
+
+
+public class Member{
+	
+	private String name;
+	private String birth;
 	private String ID;
 	private String PW;
-	private String name;
 	private String Email;
 	private int UserNumber;
-	private boolean IsMember = true;
+	boolean IsMember = false;
 	
 		
 	public String getID() {
@@ -33,6 +43,23 @@ public class Member extends User{
 		name = new_name;
 	}
 	
+	public String getbirth() {
+		return birth;
+	}
+	
+	public void setbirth(String new_birth) {
+		birth = new_birth;
+	}
+	
+		
+	public String getEmail() {
+		return Email;
+	}
+	
+	public void setEmail(String new_Email) {
+		Email = new_Email;
+	}
+	
 	public int getUserNumber() {
 		return UserNumber;
 	}
@@ -40,6 +67,58 @@ public class Member extends User{
 	public void setUserNumber(int new_UserNumber) {
 		UserNumber = new_UserNumber;
 	}
+	
+	
+	
+	public void create(String name, String birth, String ID, String PW, String Email) {
+		String input = null;
+		input = "\n" + name + " " + birth + " " + ID + " " + PW + " " + ++Application.top + " " + Email + "\n";
+		// 넣을 문자열 생성
+		
+		InputStreamReader isr = null;
+		FileWriter fw = null;
+		BufferedWriter bw = null;
+		PrintWriter pw = null;
+		
+		
+		
+		try {
+			File file = new File("member.txt");
+			bw = new BufferedWriter(new FileWriter(file, true));
+			pw = new PrintWriter(bw, true);
+			
+			pw.write(input);
+			pw.flush();
+			
+			System.out.println(input);
+		}catch(IOException e){
+			e.printStackTrace();
+			
+		}finally {
+			pw.close();
+		}
+		
+		
+		
+		
+		
+		
+	}
+	
+	public void print() {
+		System.out.printf("Name : %s\n", this.getname());
+		System.out.printf("생년월일 : %s\n", this.getbirth());
+		System.out.printf("ID : %s\n", this.getID());
+		System.out.printf("PW : %s\n", this.getPW());
+		System.out.printf("UserNumber : %d\n", this.getUserNumber());
+		System.out.printf("E-mail : %s\n", this.getEmail());
+		
+	}
+	
+	
+	
+	
+	
 	
 	
 }
